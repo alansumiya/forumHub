7397737040c18c4b242ba9b0124bdb0c9ff76a1c
Alan Sumiya <alan.sumiya@gmail.com>
2025-07-15 11:01:05 -0300

    Listagem de tópicos

--- src/main/java/br/com/alura/forumHub/controller/TopicoController.java
+++ src/main/java/br/com/alura/forumHub/controller/TopicoController.java
@@ -1,17 +1,15 @@
 package br.com.alura.forumHub.controller;
 
-import br.com.alura.forumHub.topico.DadosCadastroTopico;
-import br.com.alura.forumHub.topico.DadosDetalhamentoTopico;
-import br.com.alura.forumHub.topico.Topico;
-import br.com.alura.forumHub.topico.TopicoRepository;
+import br.com.alura.forumHub.topico.*;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
@@ -29,5 +27,24 @@ public class TopicoController {
         return ResponseEntity.created(uri).body(new DadosDetalhamentoTopico(topico));
 
     }
+
+    //opcional 1
+    @GetMapping
+    public ResponseEntity<Page<DadosListagemTopico>> listar(@PageableDefault(size = 10, sort = {"dataCriacao"}, direction = Sort.Direction.ASC) Pageable paginacao){
+        var page = repository.findAll(paginacao).map(DadosListagemTopico::new);
+        return ResponseEntity.ok(page);
+    }
+
+    //opcional 2
+    @GetMapping("/por-curso-e-ano")
+    public ResponseEntity<Page<DadosListagemTopico>> listarPorCursoEAno(
+            @RequestParam String nomeCurso,
+            @RequestParam int ano,
+            @PageableDefault(size = 10, sort = "dataCriacao", direction = Sort.Direction.ASC) Pageable paginacao
+    ){
+        var page = repository.buscarPorCursoEAno(nomeCurso, ano, paginacao).map(DadosListagemTopico::new);
+        return ResponseEntity.ok(page);
+    }
+
 
 }
--- src/main/java/br/com/alura/forumHub/topico/TopicoRepository.java
+++ src/main/java/br/com/alura/forumHub/topico/TopicoRepository.java
@@ -1,7 +1,16 @@
 package br.com.alura.forumHub.topico;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
 
+    @Query("""
+    SELECT t FROM Topico t
+    WHERE t.curso = :nomeCurso AND YEAR(t.dataCriacao) = :ano
+    """)
+    Page<Topico> buscarPorCursoEAno(@Param("nomeCurso") String nomeCurso, @Param("ano") int ano, Pageable paginacao);
 }
